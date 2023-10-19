b0c119b182007b01e1dddd50c070a502225c5dfe
Matheus Pereira Menezes <124296642+Mthh200@users.noreply.github.com>
2023-10-19 14:58:02 -0300

    Update Principal.java

--- src/br/edu/principal/Principal.java
+++ src/br/edu/principal/Principal.java
@@ -1,17 +1,15 @@
+package br.edu.principal;
+
+import modelo.*;
 public class Principal {
     public static void main(String[] args) {
         Esteira esteira = new Esteira();
         Caixa caixa = new Caixa(esteira);
 
-        esteira.ligar();
-        esteira.desligar();
-/*
         caixa.addGarrafa();
         caixa.addGarrafa();
         caixa.addGarrafa();
         caixa.addGarrafa();
-        caixa.addGarrafa();
-        caixa.addGarrafa();
-*/
+
     }
 }
